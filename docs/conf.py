fc9be657f474080618ff18edbbcecde3015222d0
Landon Bouma <landonb@users.noreply.github.com>
2023-12-16 17:57:20 -0600

    Linting: `isort` fixes

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,6 @@
 import os
 import sys
 from collections.abc import MutableMapping
-
 from importlib.metadata import version as importlib_metadata_version
 
 # import sphinx_rtd_theme
--- tests/runtests.py
+++ tests/runtests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import sys, os
+import os
+import sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 import doctest
